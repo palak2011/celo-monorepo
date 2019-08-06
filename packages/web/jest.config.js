b03ac475c481d0edb9f53a6fba795a4d27bf70e3
Aaron DeRuvo <aaron.deruvo@gmail.com>
2019-08-06 16:15:02 -0700

    still fails

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -8,4 +8,11 @@ module.exports = {
   },
   preset: './node_modules/react-native-web/jest-preset.js',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest_setup'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation|)'],
+  transform: {
+    '\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.jsx?$': 'ts-jest',
+  },
 }
